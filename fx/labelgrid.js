674b0e5143b4d07097a6042b499e2a04f764ed3d
ian29 <ian.villeda@gmail.com>
2014-09-24 16:08:17 -0400

    sketch of order by

--- fx/labelgrid.js
+++ fx/labelgrid.js
@@ -2,39 +2,61 @@ module.exports = fx;
 
 function fx(layer, options) {
     var size = options.size || null;
-    var order = options.order || null;
+    var order = options.order || '';
+    if (!order && sort) {
+        console.log(new Error('order by is not set'))
+    } else {
+        var sort = options.sort || 'asc';
+    }
+    // is this how errors work?
+    if (262144 % size !== 0) {
+        console.log(new Error(size+" does not divide evenly into 262144"));
+    }
+
+    if (layer.keys.indexOf(order) === -1) {
+        console.log(new Error("field "+order+" does not exist"));
+    }
+
+    function snap(c) {
+        return ((c%size)>size/2) ? c+(size-c%size) : c-(c%size);
+    }
+
     var grid = {}, newfeatures = [];
 
-    if (262144 % size !== 0) {
-        console.log(size, "does not divide evenly into 262144");
-    }
-
-    // to bucket features into nearest-grid-coord buckets,
-    // find the nearest multiple of `size` param
-    function snap(c) {
-        if ( (c % size) > size/2 ) {
-            // return next largest multiple of `size`
-            return c + ( size - c%size);
-        } else {
-            // return next smallest multiple of `size`
-            return c - (c%size);
+    for (var i=0; i<layer.features.length; i++) {
+        if (layer.features[i].type !== 1 ) {
+            console.log(new Error("non-point geometry"));
         }
+        var feature = layer.features[i];
+        var coordname = snap(feature.geometry[1])+','+snap(feature.geometry[2]);
+        if (!grid[coordname]) { grid[coordname] = []; }
+
+        for (var j = 0; j<feature.tags.length; j+=2) {
+            if (layer.keys[feature.tags[j]] == order) {
+                // except what if `order` is not an int_value ?
+                var orderValue = layer.values[feature.tags[j+1]].int_value;
+            }
+        }
+        grid[coordname].push([orderValue, feature])
+    }
+    // get max `order` from grid
+    // this will need to respect the sort order direction (asc vs desc)
+    for (coord in grid) {
+        // this is gross, should probably make grid[coord] an obj with 2 arrays
+        var o = 0, max = 0;
+        for (var b=0; b<grid[coord].length; b++) {
+            if (max > grid[coord][b][0]) {
+                max = grid[coord][b][0];
+                o = b;
+            }
+        }
+        newfeatures.push(grid[coord][o][1]);
     }
 
-    for (var i=0; i < layer.features.length; i++) {
-        var x = layer.features[i].geometry[1], y = layer.features[i].geometry[2];
-        var coordname = snap(x)+','+snap(y);
-        // only create gridbucket arr if any feature points live at that gridbucket
-        grid[coordname] = [];
-        // push layer feature to bucket
-        grid[coordname].push(layer.features[i]);
-    }
-    // populate new feature array with only first obj in each grid bucket
-    for ( coord in grid ) {
-        newfeatures.push(grid[coord][0]);
-    }
-
+    //console.log('old:\t', layer.features.length);
     layer.features = newfeatures;
+    //console.log('new:\t', layer.features.length);
+
     return layer;
 }
 
