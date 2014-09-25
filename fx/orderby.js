500d65059b9030ebed2d71abc7bb72da40a0cf72
ian29 <ian.villeda@gmail.com>
2014-09-25 17:27:46 -0400

    first draft of order by fx

--- fx/orderby.js
+++ fx/orderby.js
@@ -2,13 +2,28 @@ module.exports = fx;
 
 function fx(layer, options) {
     var field = options.field;
-
     if (field && layer.keys.indexOf(field) === -1) {
         console.log(new Error("field "+field+" does not exist"));
     }
     if (!field && sort) {
-        console.log(new Error('order by is not set'))
+        console.log(new Error('order by field is not set'))
     } else {
-        var sort = options.sort || 0; // 0 asc, 1 desc
+        var sort = options.sort || 1; // 0 asc, 1 desc
     }
+    // console.log(layer.features[1].id, layer.features[layer.features.length-1].id);
+    layer.features = layer.features.sort(function(a, b) {
+        for (var f = 0; f<layer.features.length; f++) {
+            var feature = layer.features[f];
+            for (var i = 0; i<feature.tags.length; i+=2) {
+                if (layer.keys[feature.tags[i]] === field) {
+                    var oa = a.layer.values[feature.tags[i+1]].int_value,
+                        ob = b.layer.values[feature.tags[i+1]].int_value;
+                    return (oa < ob) ? (-1 * sort) : (1 * sort);
+                }
+            }
+        }
+    })
+    // console.log(layer.features[1].id, layer.features[layer.features.length-1].id);
+    return layer;
+
 }
