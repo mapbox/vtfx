09137aa8e079ad9376d5196102c43e15e11fcb0b
ian29 <ian.villeda@gmail.com>
2014-09-26 12:05:03 -0400

    missing throw

--- fx/orderby.js
+++ fx/orderby.js
@@ -2,7 +2,7 @@ module.exports = fx;
 
 function fx(layer, options) {
     var field = options.field;
-    if (!field) new Error('field is not set');
+    if (!field) throw new Error('field is not set');
     if (field && layer.keys.indexOf(field) === -1) throw new Error("field "+field+" does not exist");
 
     var sort = options.sort || 1; // 1 asc, -1 desc
