690d85fa87f373c486f03ff3cf24aa702275e62b
ian29 <ian.villeda@gmail.com>
2014-10-02 14:31:34 -0400

    compare label's length to linstring length and filter

--- fx/linelabel.js
+++ fx/linelabel.js
@@ -1,45 +1,33 @@
 module.exports = fx;
 
 function fx(layer, options) {
-    var size = options.size || 10;
-    var count = 0;
-    for (var i = 0; i<layer.features.length; i++) {
-        count++;
-        var feature = layer.features[i];
-        var geoms = [];
-        // only process lines with more than 1 point
-        if (feature.type == 2) {
-            var part = [], arr = [], pos = 0 ;
-            while (pos < feature.geometry.length) {
-                part.push([feature.geometry[pos+1],feature.geometry[pos+2]]);
-                repeat = (feature.geometry[pos+3]>>3);
-                for (var d=pos+4; d<pos+4+(repeat*2); d++) {
-                    arr.push(feature.geometry[d]);
-                }
-                for (var l=0; l<arr.length; l+=2) {
-                    var x = part[part.length-1][0] + arr[l];
-                    var y = part[part.length-1][1] + arr[l+1];
-                    part.push([x,y]);
-                }
-                geoms.push(part);
-                pos = pos+5+(repeat*2);
-            }
-        }
-        var linelength = 0;
-        for (var p=0; p<geoms.length; p++) {
-            for (var g=0; g<geoms[p].length; g+=2) {
-                if (geoms[p][g] && geoms[p][g+1]) {
-                    linelength+= getlength(geoms[p][g], geoms[p][g+1]);
-                }
-            }
-        }
-        console.log(count, linelength);
-    }
-    return layer;
-}
+    var field = options.labelfield;
 
-function getlength(p1, p2) {
-    dx = p2[0] - p1[0];
-    dy = p2[1] - p1[1];
-    return Math.sqrt(dx*dx + dy*dy);
+    layer.features = layer.features.filter(function(feature){
+        var linelength = 0, pos = 0;
+        // how to only process lines?
+        //if (feature.type == 2) continue;
+        while (pos < feature.geometry.length) {
+            var partlength = 0;
+            repeat = (feature.geometry[pos+3]>>3);
+            for (var d=pos+4; d<pos+4+(repeat*2); d+=2) {
+                var dx = feature.geometry[d];
+                var dy = feature.geometry[d+1];
+                partlength+=Math.sqrt(dx*dx + dy*dy);
+            }
+            // for multi* geoms, we only care about the longest part
+            if ( partlength > linelength ) {
+                linelength = partlength;
+            }
+            pos = pos+5+(repeat*2);
+        }
+        for (var ix=0; ix<feature.tags.length; ix +=2) {
+            if (layer.keys[feature.tags[ix]] == field) {
+                var labellength = layer.values[feature.tags[ix]].string_value.length*10;
+            }
+        }
+
+        if (labellength > linelength) return true;
+    })
+    return layer;
 }
