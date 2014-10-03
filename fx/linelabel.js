de38ab5801209a46bbc6f5bd96f311d88896e960
ian29 <ian.villeda@gmail.com>
2014-10-03 14:37:55 -0400

    move this logic back to where it should be

--- fx/linelabel.js
+++ fx/linelabel.js
@@ -3,7 +3,7 @@ module.exports = fx;
 function fx(layer, options) {
     var field = options.labelfield;
     layer.features = layer.features.filter(function(feature){
-        var line = feature.type == 2 ? getlength(feature.geometry,0) : 0;
+        var line = feature.type == 2 ? getlength(feature.geometry) : 0;
         for (var ix=0; ix<feature.tags.length; ix +=2) {
             if (layer.keys[feature.tags[ix]] == field) {
                 var label = layer.values[feature.tags[ix]].string_value.length*10;
@@ -14,18 +14,19 @@ function fx(layer, options) {
     return layer;
 }
 
-function getlength(geom, ll) {
-    var pos = 0;
+function getlength(geom) {
+    var pos = 0, line = 0;
     while (pos < geom.length) {
-        var partlength = 0;
+        var part = 0;
         repeat = (geom[pos+3]>>3);
         for (var d=pos+4; d<pos+4+(repeat*2); d+=2) {
             var dx = geom[d];
             var dy = geom[d+1];
-            partlength+=Math.sqrt(dx*dx + dy*dy);
+            part+=Math.sqrt(dx*dx + dy*dy);
         }
         // for multi* geoms, we only care about the longest part
+        line = part > line ? part : line;
         pos+=5+(repeat*2);
     }
-    return partlength > ll ? partlength : ll;
+    return line;
 }
