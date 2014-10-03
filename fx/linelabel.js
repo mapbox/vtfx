96bb29d485c2d0c6cb5e4f2ddfec51940f030cd1
ian29 <ian.villeda@gmail.com>
2014-10-03 13:47:21 -0400

    simpler more concise linelabel fx

--- fx/linelabel.js
+++ fx/linelabel.js
@@ -2,32 +2,33 @@ module.exports = fx;
 
 function fx(layer, options) {
     var field = options.labelfield;
+    console.log(layer.features.length);
 
     layer.features = layer.features.filter(function(feature){
-        var linelength = 0, pos = 0;
-        // how to only process lines?
-        //if (feature.type == 2) continue;
-        while (pos < feature.geometry.length) {
-            var partlength = 0;
-            repeat = (feature.geometry[pos+3]>>3);
-            for (var d=pos+4; d<pos+4+(repeat*2); d+=2) {
-                var dx = feature.geometry[d];
-                var dy = feature.geometry[d+1];
-                partlength+=Math.sqrt(dx*dx + dy*dy);
-            }
-            // for multi* geoms, we only care about the longest part
-            if ( partlength > linelength ) {
-                linelength = partlength;
-            }
-            pos = pos+5+(repeat*2);
-        }
+        var pos = 0, line = 0;
+        line = feature.type == 2 ? getlength(pos,feature.geometry,line) : 0;
         for (var ix=0; ix<feature.tags.length; ix +=2) {
             if (layer.keys[feature.tags[ix]] == field) {
-                var labellength = layer.values[feature.tags[ix]].string_value.length*10;
+                var label = layer.values[feature.tags[ix]].string_value.length*10;
             }
         }
-
-        if (labellength > linelength) return true;
+        return (line > label);
     })
+    console.log(layer.features.length);
     return layer;
+}
+
+function getlength(pos, geom, ll) {
+    while (pos < geom.length) {
+        var partlength = 0;
+        repeat = (geom[pos+3]>>3);
+        for (var d=pos+4; d<pos+4+(repeat*2); d+=2) {
+            var dx = geom[d];
+            var dy = geom[d+1];
+            partlength+=Math.sqrt(dx*dx + dy*dy);
+        }
+        // for multi* geoms, we only care about the longest part
+        pos = pos+5+(repeat*2);
+    }
+    return partlength>ll ? partlength : ll;
 }
