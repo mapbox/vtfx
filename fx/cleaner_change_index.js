80c3d9fe185f7e93029bc87200cacae9d7c47f7b
Camille Teicheira <cteicheira@gmail.com>
2014-10-08 16:13:28 -0700

    faster loops, more accurate

--- fx/cleaner_change_index.js
+++ fx/cleaner_change_index.js
@@ -1,3 +1,5 @@
+module.exports = cleaner;
+
 // This garbage collector removes nonreferenced features from layer.keys and layer.values and
 // reindexes layer.keys and layer.values.
 // The delta between the previous index and current is calculated and
@@ -8,47 +10,57 @@
 function cleaner(layer){
     var keys = Object.keys(layer.keys).map(Number);
     var values = Object.keys(layer.values).map(Number);
-
-    var keptKeys = {};
-    var keptValues = {};
-
+    var kept = {
+        keys: {},
+        values: {}
+    };
     for (var i in layer.features){
-        for (var ix = 0; ix < layer.features[i].tags.length; ix +=2){
-            var exists = keys.indexOf(layer.features[i].tags[ix]);            
-            if (exists >= 0) {
-                keptKeys[keys[exists]] = true;
+        var featureLength = layer.features[i].tags.length;
+        for (var ix = 0; ix < featureLength; ix +=2){
+            var exists = keys.indexOf(layer.features[i].tags[ix]);
+            if (exists >= 0 && keys.length > 0) {
+                // once a key is confirmed to exist, don't let it be checked for again
+                kept.keys[keys[exists]] = true;
+                keys.splice(exists, 1);
             }
             exists = values.indexOf(layer.features[i].tags[ix+1]);
-            if (exists >= 0) {
-                keptValues[values[exists]] = true;
+            if (exists >= 0 && values.length > 0) {
+                // once a value is confirmed to exist, don't let it be checked for again
+                kept.values[values[exists]] = true;
+                values.splice(exists, 1);
             }
         }
     }
-
-    keys = Object.keys(keptKeys).map(Number);
-    values = Object.keys(keptValues).map(Number);
+    keys = Object.keys(kept.keys);
+    for (var i = 0; i < keys.length; i ++){
+        kept.keys[keys[i]] = i;
+    }
+    values = Object.keys(kept.values);
+    for (var i = 0; i < values.length; i ++){
+        kept.values[values[i]] = i;
+    }
 
     for (var i in layer.features){
-        for (var ix = 0; ix < layer.features[i].tags.length; ix +=2){
+        featureLength = layer.features[i].tags.length;
+        for (var ix = 0; ix < featureLength; ix +=2){
+            var delta;
             // offset key values for removed items
-            var delta = keys.indexOf(layer.features[i].tags[ix]);
-            delta = keys[delta] - delta;
+            delta = layer.features[i].tags[ix] - kept.keys[layer.features[i].tags[ix]];
             layer.features[i].tags[ix] = layer.features[i].tags[ix] - delta;
-            
+
             // offset value values for removed items
-            delta = values.indexOf(layer.features[i].tags[ix + 1]);
-            delta = values[delta] - delta;
+            delta = layer.features[i].tags[ix + 1] - kept.values[layer.features[i].tags[ix + 1]];
             layer.features[i].tags[ix + 1] = layer.features[i].tags[ix + 1] - delta;
         }
     }
 
     for (var i = layer.keys.length - 1; i >= 0; i--){
-        if (keptKeys[i] === undefined) layer.keys.splice(i, 1);
+        if (kept.keys[i] === undefined) layer.keys.splice(i, 1);
     }
 
     for (var i = layer.values.length - 1; i >= 0; i--){
-        if (keptValues[i] === undefined) layer.values.splice(i, 1);
+        if (kept.values[i] === undefined) layer.values.splice(i, 1);
     }
 
     return layer;
-};
+}
