3dd2387d9bdcff746d83b6cb8de8ef0475eab5a7
Camille Teicheira <cteicheira@gmail.com>
2014-10-17 16:07:32 -0400

    update linelabel test to check id

--- test/test-linelabel.js
+++ test/test-linelabel.js
@@ -19,20 +19,15 @@ tape('linelabel by name', function(assert) {
         {
             "type": "Feature",
             "geometry": {"type": "LineString","coordinates": [[0,0], [0, 9], [0,10]]},
-            "properties": {"id": 1,"name": 'r3'}
+            "properties": {"id": 2,"name": 'r3'}
         }]
     });
 
     vt.layers[0] = linelabel(vt.layers[0], {id:'linelabel', labelfield:'name'});
-
-    assert.equal(vt.layers[0].features.length, 2, 'removes line + label from features');
-    assert.equal(vt.layers[0].features[0].geometry.length, 10, 'longer line is retained');
-    assert.equal(vt.layers[0].features[1].geometry.length, 8, 'short line with short label is retained');
-
     var after = util.toGeoJSON(vt);
 
-    assert.equal(after.features[0].properties.id, 0, 'preserves order of tied features');
-    assert.equal(after.features[1].properties.id, 1, 'preserves order of tied features');
+    assert.equal(after.features[0].properties.id, 0, 'longer line is retained');
+    assert.equal(after.features[1].properties.id, 2, 'short line with short label is retained');
 
     assert.end();
 });
