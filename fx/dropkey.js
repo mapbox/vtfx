8ae00a9f68e215dd53f0ac1b259f719651b0950d
ingalls <nick@mapbox.com>
2014-10-17 16:38:08 -0400

    Update to chainable

--- fx/dropkey.js
+++ fx/dropkey.js
@@ -7,21 +7,21 @@ module.exports.parameters = {
     "name": "dropkey",
     "display": "Drop the given key",
     "description": "Drop the given key and its corresponding value from the tile",
-    "options": [ { "field": "dropkey", "type": "string" } ], "chainable": false };
+    "options": [ { "field": "field", "type": "string" } ], "chainable": true };
 
 function dropkey(layer, options) {
-    if (!options.dropkey) throw new Error('dropkey option required');
-    var keyId = layer.keys.length
-    while (keyId--)
-        if (layer.keys[keyId] === options.dropkey) break;  
-
+    if (!options.options[0].field) throw new Error('field must be given option required');
+    var keyNames = options.options.map(function (field) { return field.field });
+    var keyIds = [];
+    var l = layer.keys.length;
+    while (l--) if (keyNames.indexOf(layer.keys[l]) !== -1) keyIds.push(l);
     var featId = layer.features.length;
     while (featId--) {
         var tagId = layer.features[featId].tags.length / 2;
-        while (tagId--)
-            if (layer.features[featId].tags[tagId] === keyId) break;
-        if (tagId) layer.features[featId].tags.splice(tagId, 2);
+        while (tagId--) {
+            if (keyIds.indexOf(layer.features[featId].tags[tagId * 2]) !== -1)
+                layer.features[featId].tags.splice(tagId * 2, 2);
+        }
     }
-    
     return cleaner(layer);
 }
