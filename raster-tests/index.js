d400ace0f69381eaf1f18c9efb666517e9e3b47b
dnomadb <damon@mapbox.com>
2014-11-26 10:13:31 -0800

    a few changes

--- raster-tests/index.js
+++ raster-tests/index.js
@@ -2,27 +2,30 @@ var mapnik = require('mapnik');
 var fs = require('fs');
 var vtfx = require('../index.js');
 
-function decodeLoadImage(zxy, filepath, format) {
-    
-    var vtile = new mapnik.VectorTile(zxy.z, zxy.x, zxy.y);
+function decodeLoadImage(zxy, filepath, format, callback) {
+    try {
+        var vtile = new mapnik.VectorTile(zxy.z, zxy.x, zxy.y);
 
-    var iBuffer = fs.readFileSync(filepath);
+        var iBuffer = fs.readFileSync(filepath);
 
-    // Add the image data to a vector tile
-    vtile.addImage(iBuffer, 'raster');
+        // Add the image data to a vector tile
+        vtile.addImage(iBuffer, 'raster');
 
-    // Write the vector tile to disk
-    fs.writeFileSync('rasterbuffer-' + format + '.vector.pbf', vtile.getData());
+        // Write the vector tile to disk
+        fs.writeFileSync('rasterbuffer-' + format + '.vector.pbf', vtile.getData());
 
-    // Try decoding the image with vtfx
-    var decoded = vtfx.decode(vtile.getData());
+        // Try decoding the image with vtfx
+        var decoded = vtfx.decode(vtile.getData());
 
-    // Take the image data that was decoded with vtfx, and load it into mapnik
-    // This will fail if not an 8-bit RGB image
-    var image = mapnik.Image.fromBytesSync(decoded.layers[0].features[0].raster); 
+        // Take the image data that was decoded with vtfx, and load it into mapnik
+        // This will fail if not an 8-bit RGB image
+        var image = mapnik.Image.fromBytesSync(decoded.layers[0].features[0].raster);
 
-    // Try getting a pixel
-    return image.getPixel(100,100);
+        // Try getting a pixel
+        return callback(null, image.getPixel(100,100));
+    } catch(err) {
+        return callback(err);
+    }
 }
 
 var zxy = {
@@ -34,13 +37,19 @@ var zxy = {
 // UINT8 RGB
 // ---------
 
-var uint8query = decodeLoadImage(zxy, 'raster-tests/24989.png', 'png');
+var uint8query = decodeLoadImage(zxy, '24989.png', 'png', function(err, pData) {
+    if (err) throw err;
+    console.log(pData);
+});
 
 console.log(uint8query);
 
 // Float32 single band
 // -------------------
 
-var Float32query = decodeLoadImage(zxy, 'raster-tests/24989_ndvi.tif');
+var Float32query = decodeLoadImage(zxy, '24989_ndvi.tif', 'png', function(err, pData) {
+    if (err) throw err;
+    console.log(pData);
+});
 
 console.log(Float32query)
