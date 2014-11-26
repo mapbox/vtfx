4c0d429e41d3add1387d3da7e2d88bd422e72f32
dnomadb <damon@mapbox.com>
2014-11-25 17:23:34 -0800

    a few experiments for encoding / decoding image rasters

---
+++ raster-tests/index.js
@@ -0,0 +1,46 @@
+var mapnik = require('mapnik');
+var fs = require('fs');
+var vtfx = require('../index.js');
+
+function decodeLoadImage(zxy, filepath, format) {
+    
+    var vtile = new mapnik.VectorTile(zxy.z, zxy.x, zxy.y);
+
+    var iBuffer = fs.readFileSync(filepath);
+
+    // Add the image data to a vector tile
+    vtile.addImage(iBuffer, 'raster');
+
+    // Write the vector tile to disk
+    fs.writeFileSync('rasterbuffer-' + format + '.vector.pbf', vtile.getData());
+
+    // Try decoding the image with vtfx
+    var decoded = vtfx.decode(vtile.getData());
+
+    // Take the image data that was decoded with vtfx, and load it into mapnik
+    // This will fail if not an 8-bit RGB image
+    var image = mapnik.Image.fromBytesSync(decoded.layers[0].features[0].raster); 
+
+    // Try getting a pixel
+    return image.getPixel(100,100);
+}
+
+var zxy = {
+    z: 16,
+    x: 10642,
+    y: 24989
+};
+
+// UINT8 RGB
+// ---------
+
+var uint8query = decodeLoadImage(zxy, 'raster-tests/24989.png', 'png');
+
+console.log(uint8query);
+
+// Float32 single band
+// -------------------
+
+var Float32query = decodeLoadImage(zxy, 'raster-tests/24989_ndvi.tif');
+
+console.log(Float32query)
